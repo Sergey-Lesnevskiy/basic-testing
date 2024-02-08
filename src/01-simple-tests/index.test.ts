f28779870e92c4afe4036a32775f656c10ab43a3
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-02-08 15:23:50 +0300

    feat: add 01, 02, 03

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,28 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+const testCases = [
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 13, b: 2, action: Action.Subtract, expected: 11 },
+  { a: 31, b: 2, action: Action.Subtract, expected: 29 },
+  { a: 2, b: 2, action: Action.Divide, expected: 1 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 16, b: 2, action: Action.Divide, expected: 8 },
+  { a: 10, b: 2, action: Action.Multiply, expected: 20 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 3, b: 5, action: Action.Multiply, expected: 15 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: 4, b: 4, action: Action.Exponentiate, expected: 256 },
+];
 
-describe('simpleCalculator tests', () => {
-  test('should add two numbers', () => {
-    // Write your test here
-  });
-
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
-
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
-
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
-
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
-
-  test('should return null for invalid arguments', () => {
-    // Write your test here
-  });
+describe('simpleCalculator', () => {
+  test.each(testCases)(
+    'table with test cases',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
