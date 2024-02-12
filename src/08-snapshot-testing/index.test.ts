9f54fd15ee3fef5bd64d6fdf1ebe4f7c3eab7486
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-02-12 21:45:45 +0300

    feat: add 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,21 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+  const value = [1, 2, 3];
+  const exampleList = {
+    value: 1,
+    next: { value: 2, next: { value: 3, next: { value: null, next: null } } },
+  };
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const generatedList = generateLinkedList(value);
+    expect(generatedList).toStrictEqual(exampleList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const value = [4, 5, 6];
+    const generatedList = generateLinkedList(value);
+    expect(generatedList).toMatchSnapshot();
   });
 });
